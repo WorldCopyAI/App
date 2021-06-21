a5ecbaa420968d3f349ab48c6a25c5303b89e1eb
jeonghyeok-Lee <jeonghyeok.Lee.Dev@gmail.com>
2021-06-21 16:54:51 +0900

    이미지 전환 수정

--- app/src/main/java/com/example/tests/MainActivity.kt
+++ app/src/main/java/com/example/tests/MainActivity.kt
@@ -3,27 +3,27 @@ package com.example.tests
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private val backHandler = BackButton(this)
-    private var changeView:Boolean = false;
+    private var changeView:Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn1.setOnClickListener(){
-            if(changeView){
-                imageView2.visibility = View.INVISIBLE;
-                imageView3.visibility = View.VISIBLE;
-                changeView = true;
+        // 버튼 클릭시 이미지 변경
+        btn1.setOnClickListener{
+            if(!changeView){
+                imageView2.visibility = View.INVISIBLE
+                imageView3.visibility = View.VISIBLE
+                changeView = true
             }
             else{
-                imageView2.visibility = View.VISIBLE;
-                imageView3.visibility = View.INVISIBLE;
-                changeView = false;
+                imageView2.visibility = View.VISIBLE
+                imageView3.visibility = View.INVISIBLE
+                changeView = false
             }
         }
     }
@@ -31,4 +31,7 @@ class MainActivity : AppCompatActivity() {
     override fun onBackPressed() {
         backHandler.onBackPressed()
     }
+
+
+
 }
