fbee8f38f8dd2287be76a47a3f0afe80e9d91d10
jeonghyeok-Lee <jeonghyeok.Lee.Dev@gmail.com>
2021-06-21 14:34:58 +0900

    이미지 전환

--- app/src/main/java/com/example/tests/MainActivity.kt
+++ app/src/main/java/com/example/tests/MainActivity.kt
@@ -1,14 +1,31 @@
 package com.example.tests
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private val backHandler = BackButton(this)
+    private var changeView:Boolean = false;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn1.setOnClickListener(){
+            if(changeView){
+                imageView2.visibility = View.INVISIBLE;
+                imageView3.visibility = View.VISIBLE;
+                changeView = true;
+            }
+            else{
+                imageView2.visibility = View.VISIBLE;
+                imageView3.visibility = View.INVISIBLE;
+                changeView = false;
+            }
+        }
     }
 
     override fun onBackPressed() {
