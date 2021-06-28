7a83857d73a93c3f7942612626cfffe8d2036be5
jeonghyeok-Lee <jeonghyeok.Lee.Dev@gmail.com>
2021-06-28 13:52:51 +0900

    테스트본

--- app/src/main/java/com/example/tests/LoginActivity.kt
+++ app/src/main/java/com/example/tests/LoginActivity.kt
@@ -3,10 +3,27 @@ package com.example.tests
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedListener {
     private val backHandler = BackButton(this)
+    private lateinit var btnGoogle: SignInButton          // 구글 로그인용 버튼
+    private var auth: FirebaseAuth? = null                   // 파이어베이스 인증용 객체
+    private var googleApiClient: GoogleApiClient? = null    // 구글 API 클라이언트 객체
+
+    companion object {
+        const val REQ_SING_GOOGLE: Int = 100 // 구글 로그인 결과 코드
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,10 +34,64 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+
+        // btn_google 사용시 옵션세팅을 해주는 것 버튼클릭시에도 변하지 않는것으로 봐서는 이곳이 문제인것 같은데 도대체 뭐가 문제일지 아직은 잘모르겠음
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.firebase_web_client_id))
+            .requestEmail()
+            .build()
+
+        auth = FirebaseAuth.getInstance() // 파이어베이스 인증 객체 초기화
+        var googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
+
+        btnGoogle = findViewById(R.id.btn_google)
+        btnGoogle.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            //intent.putExtra()
+            startActivityForResult(intent, REQ_SING_GOOGLE)
+        }
+
     }
 
     override fun onBackPressed() {
         backHandler.onBackPressed()
     }
 
+    override fun onConnectionFailed(p0: ConnectionResult) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        toast("이게 실행은 되냐?")
+        if (requestCode == REQ_SING_GOOGLE) {
+            // 구글 로그인 성공시 넘어오는 토큰 값을 받음
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            // ApiException 캐치 (task 값에 있는 결과를 가져옴 -> 이넘이 구글 로그인 정보를 갖는 것 같음)
+            val account = task.getResult(ApiException::class.java)
+            // 구글 로그인에 성공했다는 인증서
+            val credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+
+            FirebaseAuth.getInstance().signInWithCredential(credential)
+                .addOnCompleteListener {
+                    if (task.isSuccessful) { //구글 로그인 성공시
+                        toast("성공")
+                        val intent = Intent(this, MainActivity::class.java)
+                        intent.putExtra("nickName", account?.getDisplayName())
+                        intent.putExtra("profile", account?.getPhotoUrl().toString())
+                        startActivity(intent)
+                    } else {// 구글 로그인 실패시
+                        toast(task.exception.toString())
+                    }
+                }
+        }
+    }
+
+    private fun toast(s: String) {
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show()
+    }
+
+
 }
+
+
--- app/src/main/java/com/example/tests/MainActivity.kt
+++ app/src/main/java/com/example/tests/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.tests
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private val backHandler = BackButton(this)
     private var changeView:Boolean = false
+    private lateinit var tv_result:TextView // 닉네임 텍스트
+    private lateinit var iv_profile:ImageView //프로필 이미지 뷰
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,21 +21,37 @@ class MainActivity : AppCompatActivity() {
 
         // 버튼 클릭시 이미지 변경
         btn1.setOnClickListener{
-            if(!changeView){
-                imageView2.visibility = View.INVISIBLE
-                imageView3.visibility = View.VISIBLE
-                changeView = true
-            }
-            else{
-                imageView2.visibility = View.VISIBLE
-                imageView3.visibility = View.INVISIBLE
-                changeView = false
-            }
+            viewChange()
         }
+
+        val intent : Intent = getIntent()
+        val nickname : String? = intent.getStringExtra("nickName") // 첫번째 창에서 닉네임을 전달받음
+        val profile : String? = intent.getStringExtra("profile") // 첫번째 창에서 프로필 사진 Url을 전달받음
+
+        tv_result = findViewById(R.id.result_name)
+        tv_result.setText(nickname)                             // 닉네임 text를 텍스트 뷰로 세팅
+
+        iv_profile = findViewById(R.id.iv_profile)
+        Glide.with(this).load(profile).into(iv_profile) // 프로필 url을 이미지뷰에 세팅한 것
+
     }
 
     override fun onBackPressed() {
         backHandler.onBackPressed()
+    }
+
+    // 이미지 변경 함수화 - 수정시 매개변수 추가해주세요
+    fun viewChange(){
+        if(!changeView){
+            imageView2.visibility = View.INVISIBLE
+            imageView3.visibility = View.VISIBLE
+            changeView = true
+        }
+        else{
+            imageView2.visibility = View.VISIBLE
+            imageView3.visibility = View.INVISIBLE
+            changeView = false
+        }
     }
 
 
