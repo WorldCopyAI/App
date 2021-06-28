b2540075e2dfcd522d076a36479fd93662806a46
jeonghyeok-Lee <jeonghyeok.Lee.Dev@gmail.com>
2021-06-28 17:04:30 +0900

    테스트본3

--- app/src/main/java/com/example/tests/LoginActivity.kt
+++ app/src/main/java/com/example/tests/LoginActivity.kt
@@ -1,18 +1,23 @@
 package com.example.tests
 
+import android.content.ContentValues.TAG
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
+
 
 class LoginActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedListener {
     private val backHandler = BackButton(this)
@@ -44,13 +49,11 @@ class LoginActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedLis
 
         btnGoogle = findViewById(R.id.btn_google)
         btnGoogle.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
 
-            var signinIntent:Intent = googleSignInClient.signInIntent
+            var signinIntent: Intent = googleSignInClient.signInIntent
             //intent.putExtra()
             startActivityForResult(signinIntent, REQ_SING_GOOGLE)
-
-            startActivity(intent)
-            val intent = Intent(this, MainActivity::class.java)
         }
 
     }
@@ -63,7 +66,38 @@ class LoginActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedLis
         TODO("Not yet implemented")
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQ_SING_GOOGLE) {
+            val task: Task<GoogleSignInAccount> =
+                GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
 
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val acct = completedTask.getResult(ApiException::class.java)
+            if (acct != null) {
+                val personName = acct.displayName
+                val personGivenName = acct.givenName
+                val personFamilyName = acct.familyName
+                val personEmail = acct.email
+                val personId = acct.id
+                val personPhoto: Uri? = acct.photoUrl
+                Log.d(TAG, "handleSignInResult:personName $personName")
+                Log.d(TAG, "handleSignInResult:personGivenName $personGivenName")
+                Log.d(TAG, "handleSignInResult:personEmail $personEmail")
+                Log.d(TAG, "handleSignInResult:personId $personId")
+                Log.d(TAG, "handleSignInResult:personFamilyName $personFamilyName")
+                Log.d(TAG, "handleSignInResult:personPhoto $personPhoto")
+            }
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.e(TAG, "signInResult:failed code=" + e.statusCode)
+        }
+    }
 
     private fun toast(s: String) {
         Toast.makeText(this, s, Toast.LENGTH_SHORT).show()
